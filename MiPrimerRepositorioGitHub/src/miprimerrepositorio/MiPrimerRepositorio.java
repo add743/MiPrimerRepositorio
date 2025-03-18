0f9ca43e11ead79d7f3b23e785ba13710364b585
Alberto Dominguez Domingo <add743@educa.madrid.org>
2025-03-18 16:08:10 +0100

    Versión segunda

--- MiPrimerRepositorioGitHub/src/miprimerrepositorio/MiPrimerRepositorio.java
+++ MiPrimerRepositorioGitHub/src/miprimerrepositorio/MiPrimerRepositorio.java
@@ -4,5 +4,6 @@ public class MiPrimerRepositorio {
 
 	public static void main(String[] args) {
 		System.out.println("Este es el primer repositorio de Alberto");
+		System.out.println("Esto es una prueba");
 	}
 }
